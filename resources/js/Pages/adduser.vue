b86fbf213897e545c4a84ad1ddb139828f517e18
ExPlo1t-php <younes_tm@outlook.com>
2023-07-17 12:30:45 +0100

    first commit

---
+++ resources/js/Pages/adduser.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>coming soon</h1>
+    <Link href="login">login</Link>
+    <Link href="register" class="ml-3">register</Link>
+    
+</template>
+<script>
+import { NProgress } from 'nprogress';
+import { Link, router } from '@inertiajs/vue3';
+export default {
+    components: {
+        Link,
+    }
+}
+</script>
